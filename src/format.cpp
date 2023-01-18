ed2f336554b74a71902308da7b617309d4ab11c4
LunaY <lightmare625@gmail.com>
2023-01-17 18:35:35 -0800

    Complete the Format function

--- src/format.cpp
+++ src/format.cpp
@@ -3,9 +3,10 @@
 #include "format.h"
 
 using std::string;
-
-// TODO: Complete this helper function
-// INPUT: Long int measuring seconds
-// OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+ 
+string Format::ElapsedTime(long seconds) 
+{ 
+    int h {seconds/3600};
+    int m {(seconds - h*3600)/60 };
+    int s {seconds - h*3600 - m *60};
+    return std::to_string(h)+":"+ std::to_string(m)+":"+ std::to_string(s); }
